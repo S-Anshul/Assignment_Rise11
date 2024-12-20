be95837b5fbe4ff457679a6d8a0c510f9cb40cab
Anshul114 <121237626+Anshul114@users.noreply.github.com>
2024-12-21 00:21:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {Routes,Route} from "react-router-dom";
+
+import Home from './components/Home';
+
+
+export default function App() {
+
+
+  
+  return (
+    <>
+
+    <Routes>
+
+    <Route path ='/' element ={<Home/>}/>
+
+ 
+
+    </Routes>
+
+    </>
+);
+}
